f1083541b19b2e4a0472187145f83d01f24caccf
David Martin Garcia <davidmartingarcia0@gmail.com>
2021-08-06 00:58:13 +0200

    adding godaddy script for DDNS setup

---
+++ update_ddns.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mydomain="${1}"
+myhostname="${2}"
+
+gdapikey="${GDAPIKEY}"
+
+myip=`curl -s "https://api.ipify.org"`
+dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
+gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
+echo $dns_data
+echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, GoDaddy DNS IP is $gdip"
+
+if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
+  echo "IP has changed!! Updating on GoDaddy"
+  curl -s -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myip}\"}]"
+  curl -v -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/CNAME/*.${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myhostname}.${mydomain}.\"}]"
+  echo "Changed IP on ${myhostname}.${mydomain} from ${gdip} to ${myip}"
+fi
