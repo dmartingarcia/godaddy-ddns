0ba2ee06277c4b34b01fdf801a8ee3bde5d29ea4
david <david@bastion>
2023-12-11 20:14:09 +0100

    adding domain in stdout logs

--- update_ddns.sh
+++ update_ddns.sh
@@ -8,7 +8,7 @@ myip=`curl -s "https://api.ipify.org"`
 dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
 gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
 echo $dns_data
-echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, GoDaddy DNS IP is $gdip"
+echo "`date '+%Y-%m-%d %H:%M:%S'` - $mydomain - Current External IP is $myip, GoDaddy DNS IP is $gdip"
 
 if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
   echo "IP has changed!! Updating on GoDaddy"
